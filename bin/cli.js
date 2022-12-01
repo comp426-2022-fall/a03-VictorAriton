853b6af9d1f26da5040c45b59f4d2ace150b8e11
VictorAriton <victorar@email.unc.edu>
2022-11-30 22:11:46 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -6,9 +6,9 @@ import roll from "../lib/roll.js"
 const args = minimist(process.argv.slice(2));
 
 
-const sides = args.sides ||6;
-const dice = args.dice  ||2;
-const rolls = args.rolls ||1;
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
 
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
