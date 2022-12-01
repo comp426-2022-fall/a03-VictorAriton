e8bf346f854a71380e12366df59279c436a17560
VictorAriton <victorar@email.unc.edu>
2022-11-30 22:22:39 -0500

    testing

--- bin/cli.js
+++ bin/cli.js
@@ -6,9 +6,9 @@ import roll from "../lib/roll.js"
 const args = minimist(process.argv.slice(2));
 
 
-const sides = args.sides || 6;
-const dice = args.dice || 2;
-const rolls = args.rolls || 1;
+const sides = args.sides;
+const dice = args.dice;
+const rolls = args.rolls;
 
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
